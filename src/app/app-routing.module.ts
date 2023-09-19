a07185464cf2d38deb501b75a0cb4cd5c3897b34
davidfer <davidfernando1112@gmail.com>
2023-09-18 19:13:24 -0500

    Feature: copia html css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
